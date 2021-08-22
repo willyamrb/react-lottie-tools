1e32033a8cd5e9f1686b9a518e3289480315bae3
willyamdev <willyamvinicius1900@gmail.com>
2021-08-21 23:36:24 -0300

    removed

--- src/@types/lottie-options.interface.ts
+++
@@ -1,7 +0,0 @@
-import { AnimationConfigWithData, AnimationConfigWithPath } from "lottie-web";
-
-interface LottieOptions
-  extends Omit<AnimationConfigWithPath, "container">,
-    Omit<AnimationConfigWithData, "container"> {}
-
-export default LottieOptions;
