01af3d7753a0f89a86cddcfe5e5a4c9a30b07fd7
willyamdev <willyamvinicius1900@gmail.com>
2021-08-22 20:24:07 -0300

    exported more types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import LottieScrollSection from "./components/lottie-scroll-section/LottieScrollSection";
 import LottieSettings from "./@types/lottie-settings.interface";
 import LottieScrollSectionProps from "./@types/lottie-scroll-section.interface";
+import LottieAnimation from "./components/lottie-animation/LottieAnimation";
+import LottieAnimationProps from "./@types/lottie-animation.interface";
+import { Timeline } from './@types/index';
 
-export { LottieScrollSection, LottieSettings, LottieScrollSectionProps };
+export {
+  LottieScrollSection,
+  LottieAnimation,
+  LottieSettings,
+  LottieScrollSectionProps,
+  LottieAnimationProps,
+  Timeline
+};
