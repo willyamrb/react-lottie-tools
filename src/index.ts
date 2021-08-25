ee95e1857b59ff508571fc26d8504d31d7fd0254
willyamdev <willyamvinicius1900@gmail.com>
2021-08-25 00:44:52 -0300

    components exported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import LottieScrollSection from "./components/lottie-scroll-section/LottieScrollSection";
+import LottieScrollAnimation from "./components/lottie-scroll-animation/LottieScrollAnimation";
 import LottieSettings from "./@types/lottie-settings.interface";
 import LottieScrollSectionProps from "./@types/lottie-scroll-section.interface";
+import LottieScrollAnimationProps from "./@types/lottie-scroll-animation.interface";
 import LottieAnimation from "./components/lottie-animation/LottieAnimation";
 import LottieAnimationProps from "./@types/lottie-animation.interface";
 import { Timeline } from './@types/index';
@@ -8,8 +10,10 @@ import { Timeline } from './@types/index';
 export {
   LottieScrollSection,
   LottieAnimation,
+  LottieScrollAnimation,
   LottieSettings,
   LottieScrollSectionProps,
+  LottieScrollAnimationProps,
   LottieAnimationProps,
   Timeline
 };
